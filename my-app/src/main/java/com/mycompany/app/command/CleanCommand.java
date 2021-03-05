324868c8b78c58173d362a05f9087c39a1a40051
jade <jade.groli@heig-vd.ch>
2021-03-05 12:38:01 +0100

    add clean command picoclo

---
+++ my-app/src/main/java/com/mycompany/app/command/CleanCommand.java
@@ -0,0 +1,19 @@
+package com.mycompany.app.command;
+
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        name = "clean",
+        description = "Picocli clean command"
+)
+
+public class CleanCommand implements Runnable {
+    public static void main(String[] args) {
+        CommandLine.run(new CleanCommand(), args);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+    }
+}
