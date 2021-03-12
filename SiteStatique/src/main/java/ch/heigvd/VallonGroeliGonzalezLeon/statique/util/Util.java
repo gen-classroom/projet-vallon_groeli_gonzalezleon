da4ad05e3f8cb3b8322b78dd916389f4a24ba44c
David González León <david.gonzalezleon@heig-vd.ch>
2021-03-12 11:45:00 +0100

    added Util

---
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/util/Util.java
@@ -0,0 +1,10 @@
+/*
+ * @File Util.java
+ * @Authors : David González León
+ * @Date 12 mars 2021
+ */
+package ch.heigvd.VallonGroeliGonzalezLeon.statique.util;
+
+public class Util {
+
+}
---
+++ SiteStatique/src/test/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/util/UtilTest.java
@@ -0,0 +1,11 @@
+package ch.heigvd.VallonGroeliGonzalezLeon.statique.util;/*
+ * @File UtilTest.java
+ * @Authors : David González León
+ * @Date 12 mars 2021
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilTest {
+
+}
