51d3a99fbfa59aa6cf6b79072ffd211ddd5aa31d
jade <jade.groli@heig-vd.ch>
2021-03-16 11:48:28 +0100

    commande init a tester

--- SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/Init.java
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/Init.java
@@ -5,15 +5,42 @@
  */
 package ch.heigvd.VallonGroeliGonzalezLeon.statique.command;
 
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
 @Command(name = "init", description = "initialize a new static site")
 public class Init implements Callable<Integer> {
+   @CommandLine.Parameters(index = "0") String sitePath;
 
    @Override
-   public Integer call() {
-      System.out.println("command init");
-      return 1;
+   public Integer call() throws IOException {
+      if (sitePath != null) {
+         File targetDirectory = new File(new File(".").getCanonicalPath() + sitePath);
+         boolean success = targetDirectory.mkdirs();
+         if (success) {
+            try {
+               File config = new File(targetDirectory + "config.json");
+               File index = new File(targetDirectory + "index.md");
+               if (config.createNewFile()) {
+                  System.out.println("File created : " + config.getName());
+               } else {
+                  System.out.println("File already exists.");
+               }
+               if (index.createNewFile()) {
+                  System.out.println("File created : " + index.getName());
+               } else {
+                  System.out.println("File already exists.");
+               }
+            } catch (IOException e) {
+               System.out.println("An error occurred");
+               e.printStackTrace();
+            }
+         }
+      }
+      return 0;
    }
 }
