b59aef2aa81874f32cc03bdae3b8882797cf00fa
David González León <david.gonzalezleon@heig-vd.ch>
2021-03-16 14:20:16 +0100

    Added Build rework

---
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/Build.java
@@ -0,0 +1,56 @@
+package ch.heigvd.VallonGroeliGonzalezLeon.statique.command;
+
+
+import ch.heigvd.VallonGroeliGonzalezLeon.statique.util.Util;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import picocli.CommandLine;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Callable;
+
+
+@CommandLine.Command(name = "build", description = "build command")
+public class Build implements Callable<Integer> {
+
+   @Override
+   public Integer call() {
+      File currentDirectory;
+      try {
+         currentDirectory = new File(new File(".").getCanonicalPath());
+      } catch (IOException e) {
+         System.err.println("Error while reading current directory");
+         e.printStackTrace();
+         return 2;
+      }
+      File buildDirectory = new File(currentDirectory.getPath() + "\\build");
+      buildDirectory.mkdir();
+      if (createIndexPage(buildDirectory, currentDirectory) != 0) {
+         return 2;
+      }
+      return 0;
+   }
+
+   private int createIndexPage(File buildDirectory, File currentDirectory) {
+      try {
+         File mdFile = new File(currentDirectory.getPath() + "/index.md");
+         if (!mdFile.isFile()) {
+            return 2;
+         }
+         String content = Util.readFile(mdFile);
+         Parser parser = Parser.builder().build();
+         Node document = parser.parse(content);
+         HtmlRenderer renderer = HtmlRenderer.builder().build();
+         File htmlFile = new File(buildDirectory.getPath() + "/index.html");
+         htmlFile.createNewFile();
+         Util.writeFile(renderer.render(document), new BufferedWriter(
+                 new OutputStreamWriter(new FileOutputStream(htmlFile), StandardCharsets.UTF_8)));
+      } catch (IOException e) {
+         System.err.println("Error while reading or writing the file");
+         e.printStackTrace();
+         return 2;
+      } return 0;
+   }
+}
--- SiteStatique/src/test/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/util/UtilTest.java
+++ SiteStatique/src/test/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/util/UtilTest.java
@@ -1,11 +1,26 @@
-package ch.heigvd.VallonGroeliGonzalezLeon.statique.util;/*
+/*
  * @File UtilTest.java
  * @Authors : David González León
  * @Date 12 mars 2021
  */
+package ch.heigvd.VallonGroeliGonzalezLeon.statique.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilTest {
+
+   @Test
+   void testWritingFileWorks() throws IOException {
+      StringWriter writer = new StringWriter();
+      String content = "This is a test to see if the writer works\n Let's hope it does.";
+      assertDoesNotThrow(() -> Util.writeFile(content, writer));
+      assertEquals(content,writer.toString());
+   }
 
 }
