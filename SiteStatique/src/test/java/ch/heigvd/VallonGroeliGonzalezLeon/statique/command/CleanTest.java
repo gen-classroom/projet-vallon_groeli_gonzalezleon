e8453d3ab54ca7fc037b4bfae212f8713ed04fff
David González León <david.gonzalez@citycable.ch>
2021-03-16 17:14:29 +0100

    beginning of tests clean

---
+++ SiteStatique/src/test/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/CleanTest.java
@@ -0,0 +1,16 @@
+package ch.heigvd.VallonGroeliGonzalezLeon.statique.command;/*
+ * @File CleanTest.java
+ * @Authors : David González León
+ * @Date 16 mars 2021
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CleanTest {
+   @Test
+   void testCleanDeletesBuildDirectory(){
+
+   }
+}
