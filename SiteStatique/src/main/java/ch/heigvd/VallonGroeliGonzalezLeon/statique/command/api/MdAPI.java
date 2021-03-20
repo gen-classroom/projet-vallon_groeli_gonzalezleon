5482899695e3a5aa07b6ea19caba0b238d95ee6f
David González León <david.gonzalez@citycable.ch>
2021-03-20 11:14:48 +0100

    Test for MdAPI and doc

--- SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/api/MdAPI.java
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/api/MdAPI.java
@@ -15,18 +15,28 @@ import java.nio.charset.StandardCharsets;
 
 public class MdAPI {
 
+   /**
+    * This function creates, from a basic markdown file index.md that must be located in currentDirectory, a html file
+    * located in the buildDirectory, that replicates the structure of the markdown file
+    *
+    * @param buildDirectory   The directory where the html file must be stored
+    * @param currentDirectory The directory where the MarkDown file is located
+    *
+    * @return - 0 if the creation of the html file was done without problem
+    *         - 1 If the file index.md does not exist in currentDirectory
+    *         - 2 if there was an issue while writing to the index.html file in the buildDirectory
+    */
    public static int createIndexPage(File buildDirectory, File currentDirectory) {
       try {
          File mdFile = new File(currentDirectory.getPath() + "/index.md");
-         if (!mdFile.isFile()) {
-            return 2;
+         if (!mdFile.exists()) {
+            return 1;
          }
          String content = Util.readFile(new BufferedReader(new InputStreamReader(new FileInputStream(mdFile))));
          Parser parser = Parser.builder().build();
          Node document = parser.parse(content);
          HtmlRenderer renderer = HtmlRenderer.builder().build();
          File htmlFile = new File(buildDirectory.getPath() + "/index.html");
-         htmlFile.createNewFile();
          Util.writeFile(renderer.render(document), new BufferedWriter(
                  new OutputStreamWriter(new FileOutputStream(htmlFile), StandardCharsets.UTF_8)));
       } catch (IOException e) {
