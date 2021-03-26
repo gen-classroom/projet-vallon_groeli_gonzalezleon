9384b5f9db3fcd6d7cfa628de6b7f50b13fe8f8b
jade <jade.groli@heig-vd.ch>
2021-03-26 15:20:30 +0100

    header init

--- SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/Init.java
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/Init.java
@@ -5,14 +5,19 @@
  */
 package ch.heigvd.VallonGroeliGonzalezLeon.statique.command;
 
+import org.apache.commons.io.FileUtils;
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
-@Command(name = "init", description = "initialize a new static site")
+
+@CommandLine.Command(name = "build", mixinStandardHelpOptions = true,
+        description = "Initializes the site and creates a file and a subfile which contains a configuration file (json) and an index file (markdown). This command needs to be" +
+                " executed where the user wants to create the repository of the site")
+
+
 public class Init implements Callable<Integer> {
    @CommandLine.Parameters(index = "0") String sitePath;
 
@@ -20,20 +25,27 @@ public class Init implements Callable<Integer> {
    public Integer call() throws IOException {
       if (sitePath != null) {
          File targetDirectory = new File(new File(".").getCanonicalPath() + sitePath);
+         if (targetDirectory.exists()) {
+            FileUtils.deleteDirectory(targetDirectory);
+         }
          boolean success = targetDirectory.mkdirs();
          if (success) {
             try {
                File config = new File(targetDirectory + "/config.json");
                File index = new File(targetDirectory + "/index.md");
-               if (config.createNewFile()) {
+
+               if (config.exists()) {
+                  System.out.println("File already exists.");
+               } else {
+                  config.createNewFile();
                   System.out.println("File created : " + config.getName());
-               } else {
-                  System.out.println("File already exists.");
                }
-               if (index.createNewFile()) {
-                  System.out.println("File created : " + index.getName());
-               } else {
+
+               if (index.exists()) {
                   System.out.println("File already exists.");
+               } else {
+                  index.createNewFile();
+                  System.out.println("File created : " + config.getName());
                }
             } catch (IOException e) {
                System.out.println("An error occurred");
