72d21635358d0266c1e32633a607437dbe6bcfdc
David González León <david.gonzalezleon@heig-vd.ch>
2021-04-17 09:42:23 +0200

    added analyseFile function

--- SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/api/MdAPI.java
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/api/MdAPI.java
@@ -6,6 +6,7 @@
 package ch.heigvd.VallonGroeliGonzalezLeon.statique.command.api;
 
 import ch.heigvd.VallonGroeliGonzalezLeon.statique.util.Util;
+import lombok.Getter;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -76,6 +77,37 @@ public class MdAPI {
          return 2;
       }
       return 0;
+   }
+
+   public static MdContent analyseFile(File mdFile) throws IOException {
+      String md = Util.readFile(new BufferedReader(new InputStreamReader(new FileInputStream(mdFile))));
+      String[] lines = md.split("\\r?\\n|\\r");
+      if (lines.length < 4) {
+         throw new IllegalArgumentException("The md file did not coincide with the defined syntax.");
+      }
+      String author = lines[1].split(":")[1];
+      String date = lines[2].split(":")[1];
+      String pageTitle = lines[0].split(":")[1];
+      StringBuilder content = new StringBuilder();
+      for (int i = 4; i < lines.length; ++i) {
+         content.append(lines[i]);
+      }
+
+      return new MdContent(content.toString(), author, date, pageTitle);
+   }
+
+   static class MdContent {
+      @Getter private final String content;
+      @Getter private final String author;
+      @Getter private final String date;
+      @Getter private final String pageTitle;
+
+      public MdContent(String content, String author, String date, String pageTitle) {
+         this.content = content;
+         this.author = author;
+         this.date = date;
+         this.pageTitle = pageTitle;
+      }
    }
 }
 
