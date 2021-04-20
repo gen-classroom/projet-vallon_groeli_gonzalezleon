2adf1082fc2b19766bbe2ac0b8f045e83588f250
jade <jade.groli@heig-vd.ch>
2021-04-20 11:45:23 +0200

    start filling layout file

--- SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/api/TemplateHTML.java
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/api/TemplateHTML.java
@@ -4,6 +4,7 @@ import ch.heigvd.VallonGroeliGonzalezLeon.statique.util.Util;
 
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.TypeSafeTemplate;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
 
@@ -24,11 +25,6 @@ public class TemplateHTML {
     public TemplateHTML(File layoutFile, File configFile) throws IOException {
         this.layoutFile = layoutFile;
         this.configFile = configFile;
-        TemplateLoader loader = new ClassPathTemplateLoaderCustom(layoutFile.getParentFile().getPath(), ".html");
-
-        Handlebars handlebars = new Handlebars(loader);
-        System.out.println("ici " + loader.getPrefix());
-        template = handlebars.compile("layout");
     }
 
     public static void initLayoutFile(File emptyFile) throws IllegalArgumentException, IOException {
@@ -37,18 +33,23 @@ public class TemplateHTML {
         }
         String defaultContent =
                 "<html lang=\"en\">\n<head>\n<meta charset=\"utf-8\">" +
-                        "\n<title>{{site.titre}}| {{page.titre}}</title>\n</head>\n<body>\n" +
+                        "\n<title> {{site.titre}} | {{page.titre}} </title>\n</head>\n<body>\n" +
                         "{%include menu.html}\n{{content }}\n</body>\n</html>";
 
         Util.writeFile(defaultContent, new FileWriter(emptyFile));
     }
 
     public String generatePage(File mdFile) throws IOException {
+        TemplateLoader loader = new ClassPathTemplateLoaderCustom(layoutFile.getParentFile().getPath(), ".html");
+        Handlebars handlebars = new Handlebars(loader);
+        template = handlebars.compile("layout");
+        System.out.println(template.text());
         Map<String, String> parameterMap = new HashMap<>();
         parameterMap.put("site.titre", "TEST POUR LE SITE");
         String templateString = template.apply(parameterMap);
         return templateString;
     }
+
 
     class ClassPathTemplateLoaderCustom extends ClassPathTemplateLoader {
 
