9ba398bc72278e8da2846a748a58c6227b2611c3
David González León <david.gonzalezleon@heig-vd.ch>
2021-05-03 10:17:59 +0200

    tests build

--- SiteStatique/src/test/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/BuildTest.java
+++ SiteStatique/src/test/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/BuildTest.java
@@ -5,8 +5,12 @@ package ch.heigvd.VallonGroeliGonzalezLeon.statique.command;/*
  */
 
 import ch.heigvd.VallonGroeliGonzalezLeon.statique.Statique;
+import ch.heigvd.VallonGroeliGonzalezLeon.statique.command.api.JsonAPI;
+import ch.heigvd.VallonGroeliGonzalezLeon.statique.command.api.MdAPI;
+import ch.heigvd.VallonGroeliGonzalezLeon.statique.command.api.TemplateHTML;
 import ch.heigvd.VallonGroeliGonzalezLeon.statique.util.Util;
 import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
@@ -18,6 +22,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BuildTest {
 
+   @AfterEach
+   void tearDown() throws IOException {
+      File testDirectory = new File(new File(".").getCanonicalPath());
+      File fileIndex = new File(testDirectory + "/index.md");
+      File fileConfig = new File(testDirectory + "/config.json");
+      File templateDir = new File(testDirectory.getPath() + "/template");
+      File buildDirectory = new File(new File(".").getCanonicalPath() + "/build");
+      fileIndex.delete();
+      fileConfig.delete();
+      FileUtils.deleteDirectory(templateDir);
+      FileUtils.deleteDirectory(buildDirectory);
+   }
 
    @Test
    void testBuildCreatesDirectory() throws IOException {
@@ -32,21 +48,30 @@ class BuildTest {
       FileUtils.deleteDirectory(buildDirectory);
    }
 
-   /*@Test
+   @Test
    void testBuildCreatesFileWithGoodContent() throws IOException {
-      File mdFile = new File(new File(".").getCanonicalPath() + "/index.md");
-      mdFile.createNewFile();
-      Util.writeFile("# Test",
-                     new BufferedWriter(new OutputStreamWriter(new FileOutputStream(mdFile), StandardCharsets.UTF_8)));
+      File testDirectory = new File(new File(".").getCanonicalPath());
+      File fileIndex = new File(testDirectory + "/index.md");
+      fileIndex.createNewFile();
+      File fileConfig = new File(testDirectory + "/config.json");
+      fileConfig.createNewFile();
+      File templateDir = new File(testDirectory.getPath() + "/template");
+      templateDir.mkdir();
+      File layout = new File(testDirectory + "/template/layout.html");
+      layout.createNewFile();
+      MdAPI.initMdIndexFile(fileIndex);
+      JsonAPI.initJSONConfigFile(fileConfig);
+      TemplateHTML.initLayoutFile(layout);
+      File buildDirectory = new File(new File(".").getCanonicalPath() + "/build");
       new CommandLine(new Statique()).execute("build");
-      File buildDirectory = new File(new File(".").getCanonicalPath() + "\\build");
-      assertTrue(buildDirectory.exists());
       File index = new File(buildDirectory.getPath() + "/index.html");
       assertTrue(index.exists());
       String content = Util.readFile(new BufferedReader(new InputStreamReader(new FileInputStream(index))));
-      assertEquals(content, "<h1>Test</h1>" + "\n");
-      mdFile.delete();
-      FileUtils.deleteDirectory(buildDirectory);
-   }*/
+      String expectedContent =
+              "<html lang=\"FR\">\n<head>\n<meta charset=\"UTF-8\">\n<title> My statique website | Mon premier " +
+              "article </title>\n</head>\n<body>\n{%include menu.html}\n<h1>Mon premier article</h1>\n<h2>Mon " +
+              "sous-titre</h2>\n<p>Le contenu de mon article.</p>\n\n</body>\n</html>";
+      assertEquals(expectedContent, content);
+   }
 
 }
