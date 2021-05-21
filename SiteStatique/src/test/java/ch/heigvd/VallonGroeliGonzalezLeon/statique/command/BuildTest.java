064f9b77f59f2bcb2d62176b9783e9706608af73
David González León <david.gonzalezleon@heig-vd.ch>
2021-05-21 12:55:06 +0200

    added test to getFileType

--- SiteStatique/src/test/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/BuildTest.java
+++ SiteStatique/src/test/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/BuildTest.java
@@ -122,4 +122,18 @@ class BuildTest {
       FileUtils.deleteDirectory(subDir);
    }
 
+   @Test
+   void fileTypeWorksCorrectly() throws IOException {
+      File testDirectory = new File(new File(".").getCanonicalPath());
+      File fileIndex = new File(testDirectory + "/index.md");
+      File fileConfig = new File(testDirectory + "/config.json");
+      File templateFile = new File(testDirectory.getPath() + "/template/layout.html");
+      assertEquals(Build.FileType.CONFIG, Build.FileType.getFileTypeFromFile(fileConfig,testDirectory));
+      assertEquals(Build.FileType.LAYOUT,Build.FileType.getFileTypeFromFile(templateFile,testDirectory));
+      assertEquals(Build.FileType.MD,Build.FileType.getFileTypeFromFile(fileIndex,testDirectory));
+      assertEquals(Build.FileType.DIRECTORY,Build.FileType.getFileTypeFromFile(testDirectory,testDirectory));
+      File tmp = new File(testDirectory+"/tmp.txt");
+      assertEquals(Build.FileType.OTHER,Build.FileType.getFileTypeFromFile(tmp,testDirectory));
+
+   }
 }
