53b89399a3f41b05e46f6b2daa39751ad1675994
jade <jade.groli@heig-vd.ch>
2021-05-28 11:10:19 +0200

    corr4

--- SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/Build.java
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/Build.java
@@ -192,7 +192,7 @@ public class Build implements Callable<Integer> {
         } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
             Path path = event.context();
             Path pathUtil = Util.generatePathInBuildDirectory(currentDir.toPath(), path);
-            File file = path.toFile();
+            File file = pathUtil.toFile();
             file.delete();
         }
     }
