0b4278d43c9b7f98532aac72553a45ed94891422
Axel Vallon <axel.vallon123@gmail.com>
2021-06-04 11:23:08 +0200

    ok

--- SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/benchmark/BenchmarkRunner.java
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/benchmark/BenchmarkRunner.java
@@ -1,5 +1,20 @@
 package ch.heigvd.VallonGroeliGonzalezLeon.statique.benchmark;
 
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 public class BenchmarkRunner {
+
+        public static void main (String ...args) throws RunnerException
+        {
+            Options options = new OptionsBuilder()
+                    .forks(1)
+                    .warmupIterations(1)
+                    .measurementIterations(5)
+                    .build();
+            new Runner(options).run();
+        }
 
 }
