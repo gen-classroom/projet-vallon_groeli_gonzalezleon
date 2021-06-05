b2d2f1c8e2adbb868ceda9d089752dd7fcc0d654
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-06-05 11:51:50 +0200

    javadoc commands except build

--- SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/Clean.java
+++ SiteStatique/src/main/java/ch/heigvd/VallonGroeliGonzalezLeon/statique/command/Clean.java
@@ -12,10 +12,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
+/**
+ * The clean command. This command cleans the local directory of the build directory if it exists
+ */
 @CommandLine.Command(name = "clean", mixinStandardHelpOptions = true,
                      description = "Cleans the given directory of any generated html files and the build directory. " +
                                    "This command needs to be executed at the root of the directory created by the " +
-                                   "init command")
+                                   "init command. If there is no build directory in the current directory, this " +
+                                   "command will do nothing and return 1.")
 public class Clean implements Callable<Integer> {
 
    @Override
@@ -30,6 +34,7 @@ public class Clean implements Callable<Integer> {
          }
       } else {
          System.err.println("Build directory does not exist, please make sure you are in the correct repository");
+         return 1;
       }
       return 0;
    }
